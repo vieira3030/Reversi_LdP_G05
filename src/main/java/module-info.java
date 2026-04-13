f6685cfe43afd7735624a7c6b3f0dc3b98a5b4ce
vieira3030 <vieirarodrigo@ipvc.pt>
2026-04-13 22:22:05 +0100

    Estrutura inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module pt.grupo05.reversi_ldp_g05 {
+    requires javafx.controls;
+    exports pt.grupo05.reversi_ldp_g05;
+}
---
+++ src/main/java/pt/grupo05/reversi_ldp_g05/SystemInfo.java
@@ -0,0 +1,13 @@
+package pt.grupo05.reversi_ldp_g05;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
